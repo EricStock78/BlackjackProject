e124a19e9fba1e720bfd67180024b9f491d34e0c
ericdstock <eric.stock@otherocean.com>
2023-03-21 15:20:16 -0230

    Added a simple method to print information about a card

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 
+def Display_Card(card):
+    print(card[0])
+    print(card[1])
+    print(card[2])
+
 def main():
     print("Blackjack Project")
+
+
+
+    card = ["Hearts", "Ace", 11];
+
+    Display_Card(card)
+
+
 
 
 # Press the green button in the gutter to run the script.
